cc40602cfe1349a6df5e583206a6f0bfc54de86e
zhaopaniOS <zhaopan@126.com>
2020-09-11 11:07:09 +0800

    增加进阶知识： gevent 。。。。。。

--- Python知识图谱/进阶知识/9.并发编程/aio.py
+++ Python知识图谱/进阶知识/9.并发编程/aio.py
@@ -10,6 +10,7 @@
 
 import asyncio
 import threading
+import time
 
 from asyncio import AbstractEventLoop
 
@@ -58,6 +59,14 @@ async def run_in_task_thread(num):
 
 # 我们期望在异步线程调用函数、并阻塞等待结果返回
 future = asyncio.run_coroutine_threadsafe(run_in_task_thread(1), loop=task_loop)
+while True:
+    # here we can see so many 'abc' string printed...
+    print('abc')
+    if future.done():
+        break
+    time.sleep(0.1)
+# test future done callback
+future.add_done_callback(lambda fu: print('future has done...'))
 print(future.result())
 # 注意这里的 threadsafe 细节
 # https://docs.python.org/3/library/queue.html
@@ -103,6 +112,44 @@ task_loop.run_in_executor(None, call_ensure_future_from_executor)
 def call_main_func_from_task_thread():
     print('call_main_func_from_task_thread')
     asyncio.run_coroutine_threadsafe(run_in_mainthread(), loop=event_loop)
+
+    async def can_run():
+        print('will run here')
+        await asyncio.sleep(0)
+    asyncio.run_coroutine_threadsafe(can_run(), loop=event_loop)
 task_loop.run_in_executor(None, call_main_func_from_task_thread)
 
-event_loop.run_forever()
+# 这里用 main loop 阻塞主线程，可以短暂的让异步任务得到执行
+event_loop.run_until_complete(asyncio.ensure_future(asyncio.sleep(0)))
+print('main loop run complete')
+
+
+def _main_loop_run():
+    try:
+        event_loop.run_forever()
+    except Exception as e:
+        print(e)
+
+# 如果我们把 event loop 放到异步线程阻塞等待会如何？
+main_loop_thread = threading.Thread(target=_main_loop_run, name='main-loop-thread')
+main_loop_thread.start()
+
+
+def trace_main_loop_task():
+    print('thread name = {}'.format(threading.current_thread().name))
+    assert threading.current_thread() is main_loop_thread
+event_loop.call_soon(trace_main_loop_task)
+
+# 通过上面的函数，可以得出结论：
+# loop 本身并不能决定协程在哪个线程被调用
+# 只有 loop 循环的代码，位于哪个线程，协程就在那个地方被回调执行
+# 也就是 loop.run_forever & run_until_complete 决定的
+
+# 思考：如何在 gevent 中保持 asyncio 正常执行，并能将数据传递回主线程执行？
+# gevent.spawn 出一个伪线程，while 等待 Queue.get
+# 异步线程的 loop.run_forever 正常阻塞
+# 异步线程收到数据 ->
+# 1. Lock: but running not on main thread
+# 2. Queue.put -> gevent 伪线程开始处理，实际上已经是主线程了，调用各种不带 async 的函数是没有问题的
+# 不论怎么处理都会有点奇怪的感觉，主要还是 gevent 与 asyncio 在同一个工程里应用导致的
+# 尽量还是选统一的方式来开发
---
+++ Python知识图谱/进阶知识/9.并发编程/ge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+@auth: zhaopan
+@time: 2020/9/9 15:41
+@desc: gevent
+"""
+
+
+import gevent
+# http://www.gevent.org/index.html
+print(gevent.__version__)
